25579d38c3e2080c3d6cb427fde95038e8bc9564
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-09-29 23:46:21 +0530

    added VideoFooter and videoSidebar

---
+++ video-sharing-frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="app">
+       <div className="app__videos">
+          <Video/>
+          <Video/>
+      </div>
+        
+        
+        
+        {/* app container */}
+    </div>
+  );
+}
+
+export default App;
