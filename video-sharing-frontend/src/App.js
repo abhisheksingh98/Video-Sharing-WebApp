b452ba0cd2c09158c24557374cd996b4bcfa246b
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-09-30 21:36:14 +0530

    setting up backend server

--- video-sharing-frontend/src/App.js
+++ video-sharing-frontend/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="app">
        <div className="app__videos">
-          <Video/>
+          <Video url = ""
+          likes = {111} shares = {222} messages = {333} channel = "abhishek" description = "this is desc" song = "Closer Chainsmokers"
+          />
           <Video/>
       </div>
         
--- video-sharing-frontend/src/Video.js
+++ video-sharing-frontend/src/Video.js
@@ -3,7 +3,7 @@ import './Video.css'
 import VideoFooter from './VideoFooter';
 import VideoSidebar from './VideoSidebar';
 
-function Video() {
+function Video({url, channel, description, song, likes, messages, shares}) {
 
     const [playing,setPlaying] = useState(false);
     const videoRef = useRef(null);
@@ -28,11 +28,11 @@ function Video() {
             className = "video_player"
             loop
             ref = {videoRef}
-            src="https://www.youtube.com/watch?v=K64S2WrclJ0&list=PLl4Y2XuUavmtPZCKAth-Ktqtpy62vjNe3&index=4&frags=wn&ab_channel=CodingBlocks">
+            src={url}>
             </video>
 
-            <VideoFooter channel = "abhishek" description = "this is desc" song = "Closer Chainsmokers" />
-            <VideoSidebar likes = {111} shares = {222} messages = {333} />
+            <VideoFooter channel={channel} description={description} song={song} />
+            <VideoSidebar likes={likes} shares={shares} messages={messages} />
 
         </div>
     )
