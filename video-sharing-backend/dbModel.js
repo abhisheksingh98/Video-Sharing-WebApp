a61c52ea02394aeb34a0474450fbfcbec9d6bb12
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-10-14 22:27:16 +0530

    modified db data, added axios, fetching data from DB

--- video-sharing-backend/dbModel.js
+++ video-sharing-backend/dbModel.js
@@ -1,15 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-//Schema defines the data structure we need to use
 const tiktokSchema = mongoose.Schema({
-    url: String,
-    channel: String,
-    song: String,
-    likes: String,
-    messages: String,
-    description: String,
-    shares: String,
-})
+  url: String,
+  channel: String,
+  description: String,
+  song: String,
+  likes: String,
+  messages: String,
+  shares: String,
+});
 
-//Collection inside the database
-export default mongoose.model('tiktokVideos',tiktokSchema);
+//Collection inside DB
+export default mongoose.model("tiktokVideos", tiktokSchema);
--- video-sharing-backend/server.js
+++ video-sharing-backend/server.js
@@ -1,56 +1,55 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import Data from './data.js'
-import data from './data.js'
-import Videos from './dbModel.js'
+import express from "express";
+import mongoose from "mongoose";
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+import data from "./data.js";
 
 //app config
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
-//middlewares
-app.use(express.json())
+//ad
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
+    next();
+});
 //DB config
-const connection_url = 'mongodb+srv://admin:8i6C6RftdGgTxXku@cluster0.y6e1m.mongodb.net/tiktok?retryWrites=true&w=majority'
-
-mongoose.connect(connection_url,{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
-
+const connection_url = "mongodb+srv://admin:8i6C6RftdGgTxXku@cluster0.y6e1m.mongodb.net/tiktok?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 //api endpoints
-app.get('/',(req,res)=> res.status(200).send('hello world'));
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
-app.get('/v1/posts',(req,res)=> res.status(200).send(Data))
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
 
-app.get('./v2/posts',(req,res)=>{
-    Videos.find((err,data)=>{
-        if(err){
-            res.status(500).send(err);
-        }
-        else {
-            res.status(200).send(data);
-        }
-    })
-})
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
-app.post('v2/posts',(req,res)=>{
-    //POST request is to ADD Data to the DB
-    //It will let us add a video doc to video collection
-    const dbVideos = req.body;
+app.post("/v2/posts", (req, res) => {
+  const dbVideos = req.body;
 
-    Videos.create(dbVideos, (err, data)=> {
-        if(err){
-            res.status(500).send(err);
-        }
-        else {
-            res.status(201).send(data);
-        }
-    })
-})
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
-
-//listener
-app.listen(port, ()=>console.log(`listening on localhost: ${port}`));
+//listen
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
